26f650593ea2f08953a2906c78e3c7e582d42e8a
bartek.skorecki <59570853+Barto670@users.noreply.github.com>
2021-12-11 20:06:48 +0000

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -3,6 +3,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'xxx');
+define('DB_USERNAME', 'xxx');
+define('DB_PASSWORD', 'xxx');
+define('DB_NAME', 'xxx');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
